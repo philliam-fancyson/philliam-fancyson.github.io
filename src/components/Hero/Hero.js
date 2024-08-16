351f5d4370d9cd5c9d2719383ec52ee1e78d9218
philliam-fancyson <philn0415@gmail.com>
2024-08-16 00:27:30 -0700

    changes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -128,13 +128,13 @@ export default function Hero() {
                         ctx.clearRect(0,0,width,height);
                         for(let i in points) {
                             // detect points in range
-                            if(Math.abs(getDistance(target, points[i])) < 8000) {
+                            if(Math.abs(getDistance(target, points[i])) < 4000) {
                                 points[i].active = 0.3;
                                 points[i].circle.active = 0.6;
-                            } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                            } else if(Math.abs(getDistance(target, points[i])) < 20000) {
                                 points[i].active = 0.1;
                                 points[i].circle.active = 0.3;
-                            } else if(Math.abs(getDistance(target, points[i])) < 80000) {
+                            } else if(Math.abs(getDistance(target, points[i])) < 70000) {
                                 points[i].active = 0.02;
                                 points[i].circle.active = 0.1;
                             } else {
